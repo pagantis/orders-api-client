0d129ade7f6b51a8b843afed740c4456a5893248
romeritoCL <romero.latorre@gmail.com>
2018-06-11 09:01:40 +0200

    updated test

--- test/Model/Order/Configuration/UrlsTest.php
+++ test/Model/Order/Configuration/UrlsTest.php
@@ -2,6 +2,7 @@
 
 namespace Test\PagaMasTarde\OrdersApiClient\Model\Order\Configuration;
 
+use PagaMasTarde\OrdersApiClient\Model\Order\Configuration\Urls;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -10,4 +11,103 @@ use PHPUnit\Framework\TestCase;
  */
 class UrlsTest extends TestCase
 {
+    /**
+     * Invalid URL
+     */
+    const VALID_URL = 'http://pagamastarde.com:8080/api/v1/orders?order=true';
+
+    /**
+     *  Valid URL
+     */
+    const INVALID_URL = '://pay.es';
+
+    /**
+     * testUrlValidate
+     */
+    public function testUrlValidate()
+    {
+        $this->assertTrue(Urls::urlValidate('https://pagamastarde.com'));
+        $this->assertTrue(Urls::urlValidate('http://pagamastarde.com:8080/api/v1/orders?order=true'));
+        $this->assertFalse(Urls::urlValidate('://google.es'));
+        $this->assertFalse(Urls::urlValidate('google.es'));
+        $this->assertFalse(Urls::urlValidate('google'));
+    }
+
+    /**
+     * testSetOk
+     *
+     * @expectedException \Exceptions\Data\ValidationException
+     */
+    public function testValidate()
+    {
+        //Expected true:
+        $urls = new Urls();
+        $urls->setKo(self::VALID_URL);
+        $urls->setOk(self::VALID_URL);
+        $this->assertTrue($urls->validate());
+
+        //Expected Failure:
+        $urls = new Urls();
+        $this->assertTrue($urls->validate());
+    }
+
+    /**
+     * testSetOk
+     *
+     * @expectedException \Exceptions\Data\ValidationException
+     */
+    public function testSetOk()
+    {
+        $urls = new Urls();
+        $urls->setOk(null);
+        $this->assertNull($urls->getOk());
+        $urls->setOk(self::VALID_URL);
+        $this->assertEquals(self::VALID_URL, $urls->getOk());
+        $urls->setOk(self::INVALID_URL);
+    }
+
+    /**
+     * testSetOk
+     *
+     * @expectedException \Exceptions\Data\ValidationException
+     */
+    public function testSetKo()
+    {
+        $urls = new Urls();
+        $urls->setKo(null);
+        $this->assertNull($urls->getKo());
+        $urls->setKo(self::VALID_URL);
+        $this->assertEquals(self::VALID_URL, $urls->getKo());
+        $urls->setKo(self::INVALID_URL);
+    }
+
+    /**
+     * testSetOk
+     *
+     * @expectedException \Exceptions\Data\ValidationException
+     */
+    public function testSetCancel()
+    {
+        $urls = new Urls();
+        $urls->setCancel(null);
+        $this->assertNull($urls->getCancel());
+        $urls->setCancel(self::VALID_URL);
+        $this->assertEquals(self::VALID_URL, $urls->getCancel());
+        $urls->setCancel(self::INVALID_URL);
+    }
+
+    /**
+     * testSetOk
+     *
+     * @expectedException \Exceptions\Data\ValidationException
+     */
+    public function testSetNotificationCallback()
+    {
+        $urls = new Urls();
+        $urls->setNotificationCallback(null);
+        $this->assertNull($urls->getNotificationCallback());
+        $urls->setNotificationCallback(self::VALID_URL);
+        $this->assertEquals(self::VALID_URL, $urls->getNotificationCallback());
+        $urls->setNotificationCallback(self::INVALID_URL);
+    }
 }
