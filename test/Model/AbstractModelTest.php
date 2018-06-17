80625b5de36f50415cbcc82bd1986ee17e8dcaaa
romeritoCL <romero.latorre@gmail.com>
2018-06-17 10:21:56 +0200

    Updated compatibility with 5.3

--- test/Model/AbstractModelTest.php
+++ test/Model/AbstractModelTest.php
@@ -19,14 +19,29 @@ class AbstractModelTest extends TestCase
      */
     public function testAllMethod()
     {
-        $orderJson = file_get_contents('test/Resources/Order.json');
-        $object = json_decode($orderJson);
-        $order = new Order();
-        $order->import($object);
-        $this->assertTrue($order->validate());
-        $orderExport = json_decode(json_encode($order->export()));
-        $orderExportJson = json_encode($order->export());
-        $this->assertEquals($object, $orderExport);
-        $this->assertEquals($orderJson, $orderExportJson);
+        if (version_compare(phpversion(), '5.4', '>=')) {
+            $orderJson = file_get_contents('test/Resources/Order.json');
+            $object = json_decode($orderJson);
+            $order = new Order();
+            $order->import($object);
+            $this->assertTrue($order->validate());
+            $orderExport = json_decode(json_encode($order->export()));
+            $orderExportJson = json_encode(
+                $order->export(),
+                JSON_UNESCAPED_UNICODE |
+                JSON_PRETTY_PRINT |
+                JSON_UNESCAPED_SLASHES
+            );
+            $this->assertEquals($object, $orderExport);
+            $this->assertEquals($orderJson, $orderExportJson);
+        } else {
+            $orderJson = file_get_contents('test/Resources/Order.json');
+            $object = json_decode($orderJson);
+            $order = new Order();
+            $order->import($object);
+            $this->assertTrue($order->validate());
+            $orderExport = json_decode(json_encode($order->export()));
+            $this->assertEquals($object, $orderExport);
+        }
     }
 }
