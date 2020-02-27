d7855daffd043aadef024bad5790e836efab9347
pgarces <gspablo@gmail.com>
2020-02-27 15:43:07 +0100

    Add new field dni

--- src/Model/Order/User/Address.php
+++ src/Model/Order/User/Address.php
@@ -36,6 +36,11 @@ class Address extends AbstractModel
     protected $zipCode;
 
     /**
+     * @var string $dni the DNI of the user.
+     */
+    protected $dni;
+
+    /**
      * @var string $fixPhone Fix Phone of the user
      */
     protected $fixPhone;
@@ -151,6 +156,26 @@ class Address extends AbstractModel
     public function setZipCode($zipCode)
     {
         $this->zipCode = $zipCode;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDni()
+    {
+        return $this->dni;
+    }
+
+    /**
+     * @param string $dni
+     *
+     * @return Address
+     */
+    public function setDni($dni)
+    {
+        $this->dni = $dni;
 
         return $this;
     }
