6f5dc941edc6fca873646efd14e8e3a0cdf9a15a
tmeliotpg <tmeliot@pagantis.com>
2020-03-23 14:44:16 +0100

    saving work in progress

---
+++ examples/index.php
@@ -0,0 +1,15 @@
+
+
+<html lang="en">
+<body>
+<h2>The select desired method</h2>
+<form action="runTest.php" method="POST">
+    <label for="custom_method">Choose a method:</label>
+    <select id="custom_method" name="custom_method">
+        <option value="createOrder">create Order</option>
+    </select>
+    <input type="submit" name="submit">
+</form>
+
+</body>
+</html>
